a07a126e00f374f49bc1ad2e1964ec33337a3b77
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-07-08 10:29:36 -0500

    Fix export

--- src/services/mealsdb.js
+++ src/services/mealsdb.js
@@ -1,6 +1,6 @@
 // import Axios from 'axios';
 
-const BASE = 'https://www.themealdb.com/api/json/v1/1/';
+export const BASE = 'https://www.themealdb.com/api/json/v1/1/';
 export const LOOKUP_CATS = 'categories.php';
 export const LOOKUP_CAT = 'filter.php?c=';
 export const LOOKUP_ID = 'lookup.php?i=';
