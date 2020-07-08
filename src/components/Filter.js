b2ed0ea7a9dc4d8288022e3b12d08863f8dc1a31
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-07-08 15:18:05 -0500

    Display categories received as props to filter

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
-const Filter = ({ handleFilter }) => {
+const Filter = ({ handleFilter, categories }) => {
 
   return (
     <aside>
       <h3>Categories</h3>
       <ul>
-        <li onClick={handleFilter}>Seafood</li>
+        {
+          categories.map(category => {
+            return <li key={category.idCategory} onClick={handleFilter}>
+              {category.strCategory}
+            </li>
+          })
+        }
       </ul>
     </aside>
   )
