c9d3697a4ae99468a9f1af878be265372ccea3f9
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-07-07 19:51:29 -0500

    Insert footer and header components

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Cataloge from '../containers/Catalogue';
 import Catalogue from '../containers/Catalogue';
 
 function App() {
--- src/containers/Catalogue.js
+++ src/containers/Catalogue.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import Recipe from '../components/Recipe';
+// import Recipe from '../components/Recipe';
 
 const Catalogue = (props) => {
   return (
-    <h2>Hey</h2>
+    <>
+      <Header />
+      <Footer />
+    </>
   )
 };
 
