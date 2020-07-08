e019b113a535c65a2addb3ddcfced8d9ad288d20
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-07-07 19:50:59 -0500

    Create footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer>
+    <ul>
+      <li key="copy">Website developed by: <a href="https://sebgil.me">Sebastian Gil</a></li>
+      <li key="repo">View GitHub repository</li>
+    </ul>
+  </footer>
+);
+
+export default Footer;
