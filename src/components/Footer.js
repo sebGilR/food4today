9c359847dce9e960fc6e29ce55829ce8a18c1e29
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-07-07 20:20:35 -0500

    Fix link target in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Footer = () => (
   <footer>
     <ul>
-      <li key="copy">Website developed by: <a href="https://sebgil.me">Sebastian Gil</a></li>
+      <li key="copy">
+        Website developed by:
+        <a href="https://sebgil.me" target="_blank" rel="noopener noreferrer">Sebastian Gil</a>
+      </li>
       <li key="repo">View GitHub repository</li>
     </ul>
   </footer>
