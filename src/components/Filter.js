01e3465cea56b20c1cdda7185d9fd91ab6a144eb
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-07-07 20:39:36 -0500

    Create Filter component

---
+++ src/components/Filter.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Filter = () => {
+
+  return (
+    <aside>
+      <h3>Categories</h3>
+      <ul>
+        <li>Category</li>
+      </ul>
+    </aside>
+  )
+}
+
+export default Filter;
--- src/containers/Catalogue.js
+++ src/containers/Catalogue.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Filter from '../components/Filter';
 // import Recipe from '../components/Recipe';
 
 const Catalogue = (props) => {
   return (
     <>
       <Header />
+      <Filter />
       <Footer />
     </>
   )
