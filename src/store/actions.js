96c72803c504c39379e68debad50c034f02c38fe
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-07-07 22:12:24 -0500

    Add fetch actions and update reducer with new action types

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,6 +1,19 @@
 const filterRecipes = category => ({
-  type: "FILTER",
-  category
+  type: 'FILTER',
+  category,
 });
+
+export const fetchInit = () => ({
+  type: 'STORIES_FETCH_INIT',
+});
+
+export const fetchSuccess = data => ({
+  type: 'STORIES_FETCH_SUCCESS',
+  payload: data.meals,
+});
+
+export const fetchFailure = () => ({
+  type: 'STORIES_FETCH_FAILURE',
+})
 
 export default filterRecipes;
--- src/store/recipesReducer.js
+++ src/store/recipesReducer.js
@@ -1,8 +1,27 @@
 const recipesReducer = (state = [], action) => {
-  if (action.type === 'FILTER') {
-    return action.category
+  switch (action.type) {
+    case 'STORIES_FETCH_INIT':
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case 'STORIES_FETCH_SUCCES':
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload,
+      };
+    case 'STORIES_FETCH_FAILURE':
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    default:
+      return state;
   }
-  return state;
 }
 
 export default recipesReducer;
