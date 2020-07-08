b998115070a425385282b59f966e493ebaa9e9d0
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-07-08 10:27:22 -0500

    Update store defaults

--- src/index.js
+++ src/index.js
@@ -6,7 +6,12 @@ import './index.css';
 import App from './components/App';
 import rootReducer from './store/reducers';
 
-const store = createStore(rootReducer, { data: { recipes: [], isLoading: false, isError: false } },
+const store = createStore(rootReducer,
+  {
+    data: { recipes: [], isLoading: false, isError: false },
+    url: '',
+    filter: '',
+  },
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
