67d2d1c52a8362df45d221b93dc49df6bc4e5a32
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-07-09 14:18:15 -0500

    Style the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import style from '../assets/styles/Footer.module.scss';
 
 const Footer = () => (
-  <footer>
-    <ul>
+  <footer className={style.container}>
+    <ul className={style.list}>
       <li key="copy">
-        Website developed by:
-        <a href="https://sebgil.me" target="_blank" rel="noopener noreferrer">Sebastian Gil</a>
+        Developed by:
+        <a href="https://sebgil.me" target="_blank" rel="noopener noreferrer"> Sebastian Gil</a>
       </li>
-      <li key="repo">View GitHub repository</li>
+      <li key="repo">
+        <a href="https://github.com/sebGilR" target="_blank" rel="noopener noreferrer">GitHub</a>
+      </li>
     </ul>
   </footer>
 );
