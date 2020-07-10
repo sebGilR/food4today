e0e71ac3472ad8272336886bfaf8820c93330236
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-07-10 11:13:17 -0500

    Test categories reducer

---
+++ src/tests/categoriesReducer.test.js
@@ -0,0 +1,13 @@
+import { getCategories } from '../store/actions';
+import categoriesReducer from '../store/reducers/categoriesReducer';
+
+describe('categoriesReducer', () => {
+  it(`returns the action payload (array of categories)
+    when passed an action of type "CATEGORIES"`, () => {
+    expect(categoriesReducer(null, getCategories([]))).toEqual([])
+  });
+
+  it('returns the state if the action is not "CATEGORIES"', () => {
+    expect(categoriesReducer('STATE', { type: "OTHER" })).toEqual('STATE')
+  });
+})
